e70a44692d7b3ce93edab0495ef52a5e92454767
shivshankar-source <shivshakti.ss771@gmail.com>
2025-11-11 13:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+let submitButton = document.getElementById("submit");
+let existingButton = document.getElementById("existing");
+let checkbox = document.getElementById("checkbox");
+let usernameInput = document.getElementById("username");
+let passwordInput = document.getElementById("password");
+
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("data")) {
+    existingButton.hidden = false;
+  }
+});
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let username = usernameInput.value;
+  let password = passwordInput.value;
+
+  alert(`Logged in as ${username}`);
+
+  if (checkbox.checked) {
+    let data = { username, password };
+    localStorage.setItem("data", JSON.stringify(data));
+  } else {
+    localStorage.removeItem("data");
+  }
+
+
+  if (localStorage.getItem("data")) {
+    existingButton.hidden = false;
+  } else {
+    existingButton.hidden = true;
+  }
+});
+
+existingButton.addEventListener("click", function () {
+  let data = JSON.parse(localStorage.getItem("data"));
+  alert(`Logged in as ${data.username}`);
+});
